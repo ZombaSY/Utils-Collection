67e32ed5b2be7d162782ff1cccf2526578b4afe2
Sunyong Seo <tjxhwl@gmail.com>
2020-10-16 18:00:11 +0900

    update image_plot

--- image processing/image_plot.py
+++ image processing/image_plot.py
@@ -1,46 +1,93 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL.Image as pil
-from PIL.Image import BILINEAR, BICUBIC
 import os
+import math
 
-real_in_dir = 'A:/Users/SSY/Documents/GitHub/PyTorch-CycleGAN/datasets/image_calibration/test/A'
-real_out_dir = 'A:/Users/SSY/Documents/GitHub/PyTorch-CycleGAN/datasets/image_calibration/test/B'
-fake_out_dir = 'A:/Users/SSY/Documents/GitHub/PyTorch-CycleGAN/output/B'
+from PIL.Image import BILINEAR, BICUBIC
 
-real_in_list = os.listdir(real_in_dir)
-real_out_list = os.listdir(real_out_dir)
-fake_out_list = os.listdir(fake_out_dir)
-
-image_show_row = 50
-
-plt_images = list(zip(real_in_list, real_out_list, fake_out_list))
+"""
+:Usage
+ 1. set 'real_in_dir', 'real_out_dir', 'fake_out_dir'
+ 2. set 'image_show_row' to limit the maximum row in plots
+ 3. if "output image" needs resize the same ratio of "input or target image", release annotation near line 62
+"""
 
 plt.axis('off')
-fig = plt.figure(figsize=(image_show_row*2/5, image_show_row*2))
+figure_dpi = 300
 
-for idx, image_zip in enumerate(plt_images[:image_show_row]):
-    real_in = os.path.join(real_in_dir, image_zip[0])
-    real_out = os.path.join(real_out_dir, image_zip[1])
-    fake_out = os.path.join(fake_out_dir, image_zip[2])
+# limit the maximum row to avoid 'pyplot plot maximum size exception'.
+PLT_ROW = 50
+PLT_COL = 3
 
-    f_real_in = pil.open(real_in)
-    f_real_out = pil.open(real_out)
-    f_fake_out = pil.open(fake_out)
+input_dir = 'A:/Users/SSY/Desktop/dataset/cud_calibration/201012 dataset/train/A'
+output_dir = 'A:/Users/SSY/Desktop/dataset/cud_calibration/201012 dataset/train/A'
+target_dir = 'A:/Users/SSY/Desktop/dataset/cud_calibration/201012 dataset/train/B'
 
-    w, h = f_real_out.size
-    f_fake_out = f_fake_out.resize((w, h), BICUBIC)
+input_list = os.listdir(input_dir)
+output_list = os.listdir(output_dir)
+target_list = os.listdir(target_dir)
 
-    for i in range(2, -1, -1):
-        ax = fig.add_subplot(image_show_row, 3, (idx+1)*3 - i)
-        if i == 0:
-            ax.imshow(f_fake_out)
-        if i == 1:
-            ax.imshow(f_real_out)
-        if i == 2:
-            ax.imshow(f_real_in)
-        ax.axis('off')
+assert len(input_list) == len(output_list) == len(target_list), 'length of images in directory should be same'
 
+image_show_row = len(input_list)
+# image_show_row = 10
 
-fig.savefig('zzz.jpg', dpi=240)
+plt_images = list(zip(input_list, output_list, target_list))
 
+render_iteration = 1
+if image_show_row > PLT_ROW:
+    render_iteration = math.ceil(image_show_row / PLT_ROW)
+
+for render_idx in range(render_iteration):
+    last_index = render_idx * PLT_ROW
+
+    figure_width = 1280 / figure_dpi
+    figure_height = 1080 / figure_dpi
+
+    if not (render_idx + 1 == render_iteration):
+        plt_render_images = plt_images[last_index:PLT_ROW * (render_idx + 1)]
+
+        fig = plt.figure(figsize=(figure_width * PLT_COL, figure_height * PLT_ROW))
+        fig.dpi = figure_dpi
+    else:
+        plt_render_images = plt_images[last_index:image_show_row]
+
+        _, lefts = divmod(image_show_row, PLT_ROW)
+        fig = plt.figure(figsize=(figure_width * PLT_COL, figure_height * lefts))
+        fig.dpi = figure_dpi
+
+    for idx, image_zip in enumerate(plt_render_images):
+        input_img_dr = os.path.join(input_dir, image_zip[0])
+        output_img_dr = os.path.join(output_dir, image_zip[1])
+        target_img_dr = os.path.join(target_dir, image_zip[2])
+
+        input_img = pil.open(input_img_dr)
+        output_img = pil.open(output_img_dr)
+        target_img = pil.open(target_img_dr)
+
+        w, h = input_img.size
+        # output_img = input_img.resize((w, h), BICUBIC)   # if needs resize
+
+        for i in range(2, -1, -1):
+            ax = fig.add_subplot(image_show_row, PLT_COL, (idx+1) * PLT_COL - i)
+            if i == 2:
+                ax.imshow(input_img)
+            if i == 1:
+                ax.imshow(output_img)
+            if i == 0:
+                ax.imshow(target_img)
+            ax.axis('off')
+
+        print('finished plotting {}th image'.format((render_idx * PLT_ROW) + (idx + 1)))
+
+    print('\nmaking image... wait for seconds')
+
+    if not os.path.exists('./image_plot'):
+        os.mkdir('./image_plot')
+
+    fn = './image_plot/output_' + str(render_idx).zfill(3) + '.jpg'
+    fig.savefig(fn, dpi=fig.dpi)
+    print('saved', fn, end='\n\n')
+
+print('Done!!!')
--- image processing/image_resize.py
+++ image processing/image_resize.py
@@ -3,13 +3,11 @@ from PIL import Image
 import math
 import os
 
-DATASET_PATH = 'A:/Users/SSY/Desktop/dataset/cud_calibration/RAW/학습데이터(T3_81-131)_201008 전달/A/'
+DATASET_PATH = 'A:/Users/SSY/Desktop/dataset/cud_calibration/201012 dataset/train/A/'
 MAXIMUM_RESOLUTION = 1280*720
 
 
-def img_resize(src_path, fn, maximum_resolution):
-    img: Image.Image = Image.open(src_path)
-
+def img_resize(img, maximum_resolution):
     img_width = img.width
     img_height = img.height
     img_definition = img_width * img_height
@@ -25,19 +23,22 @@ def img_resize(src_path, fn, maximum_resolution):
 
         img = img.resize((img_width_r, img_height_r))
 
-    if not os.path.exists('./image_resize'):
-        os.mkdir('.image_resize')
-
-    img.save('./image_resize/' + fn, quality=100, dpi=img_dpi)
-    print(fn + ' Done!')
+    return img, img_dpi
 
 
 def main():
     file_list = os.listdir(DATASET_PATH)
 
+    if not os.path.exists('./image_resize'):
+        os.mkdir('.image_resize')
+
     for idx, fn in enumerate(file_list):
         img_path = DATASET_PATH + fn
-        img_resize(img_path, fn, maximum_resolution=MAXIMUM_RESOLUTION)
+        img = Image.open(img_path)
+        img, dpi = img_resize(img, maximum_resolution=MAXIMUM_RESOLUTION)
+
+        img.save('./image_resize/' + fn, quality=100, dpi=dpi)
+        print(fn + ' Done!')
 
 
 if __name__ == '__main__':
